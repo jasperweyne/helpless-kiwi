d2a31a9f18df18c1caf26a40ddd7e02d54963f08
Jasper Weyne <jasperweyne@gmail.com>
2019-07-17 17:26:53 +0200

    Fix admin bugs for activities

--- src/Entity/Activity/Activity.php
+++ src/Entity/Activity/Activity.php
@@ -4,7 +4,7 @@ namespace App\Entity\Activity;
 
 use App\Entity\Activity\PriceOption;
 use App\Entity\Group\Taxonomy;
-use App\Entity\Location;
+use App\Entity\Location\Location;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
