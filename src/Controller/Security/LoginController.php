211e253e16a15159bf88aaabc135ceada2e5551d
Jasper Weyne <jasperweyne@gmail.com>
2019-07-24 01:43:45 +0200

    Fix login

--- src/Controller/Security/LoginController.php
+++ src/Controller/Security/LoginController.php
@@ -32,7 +32,7 @@ class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername]);
     }
 
     /**
