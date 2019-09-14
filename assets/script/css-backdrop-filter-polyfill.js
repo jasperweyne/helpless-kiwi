836ee337f537d163ab73b54d665a2c73569b3c99
Jasper Weyne <jasperweyne@gmail.com>
2019-09-14 02:04:28 +0200

    Fixed backdrop-filter polyfill for absolute positions and background colors

--- assets/script/css-backdrop-filter-polyfill.js
+++ assets/script/css-backdrop-filter-polyfill.js
@@ -1,7 +1,61 @@
 require('./css-supports-polyfill.js');
 require('./polyfill.js');
+// var backdropjs = require('backdropjs');
+// const toPx = require('to-px');
 
 var domtoimage = require('dom-to-image');
+
+// var addRule = (function (style) {
+//     var sheet = document.head.appendChild(style).sheet;
+//     return function (selector, css) {
+//         var propText = typeof css === "string" ? css : Object.keys(css).map(function (p) {
+//             return p + ":" + (p === "content" ? "'" + css[p] + "'" : css[p]);
+//         }).join(";");
+//         sheet.insertRule(selector + "{" + propText + "}", sheet.cssRules.length);
+//     };
+// })(document.createElement("style"));
+
+
+// function backdrop(backdropsource, backdropapply, blur, scroller) {
+//     if (document.getElementById("iniframe") == null) {
+//         backdropframe = document.createElement("iframe")
+//         backdropframe.style.border = "none"
+//         backdropframe.width = "100%"
+//         backdropframe.height = "100%"
+//         var node = backdropapply.appendChild(backdropframe)
+//         addbackdrop()
+
+//         return node;
+//     }
+  
+//     function addbackdrop() {
+//         backdropdoc = backdropframe.contentWindow.document
+//         backdropdoc.open()
+//         backdropdoc.write(backdropsource)
+//         var markup =
+//             '<div id="iniframe"></div><style>body {overflow: hidden; -webkit-filter: blur(' +
+//             blur +
+//             "px);filter: blur(" +
+//             blur +
+//             "px); -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none;}</style>"
+//         backdropdoc.write(markup)
+//         backdropdoc.close()
+//         if (scroller == "yes") {
+//             window.addEventListener(
+//             "scroll",
+//             function(event) {
+//                 var scroll = this.scrollY
+//                 var scrollx = this.scrollX
+//                 var rect = backdropapply.getBoundingClientRect()
+//                 scroll = scroll - rect.top
+//                 backdropframe.contentWindow.window.scrollTo(scrollx, scroll)
+//             },
+//             true
+//             )
+//         }
+//     }
+// }
+
 
 if (!window.CSS.supports('backdrop-filter')) {
     // List of all elements with 'backdrop-filter' on page
@@ -30,20 +84,19 @@ if (!window.CSS.supports('backdrop-filter')) {
             
             document.querySelectorAll(rule.getSelectors()).forEach(function(node) {
                 var bgNode = document.createElement('div');
+                var isFixed = window.getComputedStyle(node).getPropertyValue('position') == 'fixed';
                 Object.assign(bgNode.style, {
-                    'position': 'absolute',
-                    'top': '0',
-                    'left': '0',
-                    'width': '100%',
-                    'height': '100%',
-                    'z-index': '-9999',
+                    'position': isFixed ? 'fixed' : 'absolute',
+                    'top': node.offsetTop,
+                    'left': node.offsetLeft,
+                    'width': node.offsetWidth + 'px',
+                    'height': node.offsetHeight + 'px',
                     '-webkit-filter': filterVal,
                     '-moz-filter': filterVal,
                     'filter': filterVal
                 })
-                node.appendChild(bgNode);
+                node.parentNode.insertBefore(bgNode, node);
 
-                var isFixed = window.getComputedStyle(node).getPropertyValue('position') == 'fixed';
                 if (isFixed) {
                     window.addEventListener(
                         "scroll",
@@ -52,7 +105,7 @@ if (!window.CSS.supports('backdrop-filter')) {
                             var scrollx = this.scrollX
                             var rect = node.getBoundingClientRect()
                             scroll = scroll - rect.top
-                            bgNode.style.backgroundPosition = (-1 * node.offsetLeft - scrollx) + 'px ' + (-2 * node.offsetTop - scroll) + 'px';
+                            bgNode.style.backgroundPosition = (-scrollx) + 'px ' + (-2 * node.offsetTop - scroll) + 'px';
                         },
                         true
                     )
@@ -70,6 +123,7 @@ if (!window.CSS.supports('backdrop-filter')) {
             // first, hide all nodes
             elems.forEach(function(elem) {
                 elem.node.style.opacity = 0;
+                elem.bgNode.style.opacity = 0;
             });
             fixed.forEach(function(elem) {
                 elem.node.style.opacity = 0;
@@ -85,8 +139,9 @@ if (!window.CSS.supports('backdrop-filter')) {
                 // now, restore opacity of each node and set blurred image
                 elems.forEach(function(elem) {
                     elem.node.style.opacity = elem.originalOpacity;
+                    elem.bgNode.style.opacity = 1;
                     Object.assign(elem.bgNode.style, {
-                        'background': 'url(' + dataUrl + ') no-repeat ' + (-1 * elem.node.offsetLeft) + 'px ' + (-1 * elem.node.offsetTop) + 'px',
+                        'background': 'url(' + dataUrl + ') no-repeat 0px ' + (-1 * elem.node.offsetTop) + 'px',
                     });
                 });
             });
@@ -94,5 +149,84 @@ if (!window.CSS.supports('backdrop-filter')) {
     
         updateBg();
         window.addEventListener("resize", updateBg);
+
+        // rules.each(function(rule) {
+        //     var filterVal = rule.getDeclaration()['backdrop-filter'];
+        //     var extracted = filterVal.match(/blur\((\w+)\)/)[1];
+            
+        //     document.querySelectorAll(rule.getSelectors()).forEach(function(elem) {
+        //         // backdropjs.backdrop(document.documentElement.outerHTML, elem, toPx(extracted), "yes");
+
+        //         // var blurParent = (function() {
+        //         //     var node = document.createElement("div");
+        //         //     Object.assign(node.style, {
+        //         //         'position': 'absolute',
+        //         //         'display': 'block',
+        //         //         'overflow': 'hidden',
+        //         //         'visibility': 'hidden',
+        //         //         'width': '100%',
+        //         //         'height': '100%',
+        //         //         'top': '0',
+        //         //         'left': '0',
+        //         //         'z-index': '999999',
+        //         //         '-webkit-filter': filterVal,
+        //         //         '-moz-filter': filterVal,
+        //         //         'filter': filterVal,
+        //         //     });
+        //         //     return document.body.appendChild(node);
+        //         // })();
+
+                
+        //         // addRule(rule.getSelectors() + ":before", {
+        //         //     'visibility': 'visible',
+        //         //     'content': '',
+        //         //     'position': 'absolute',
+        //         //     'width': '100%',
+        //         //     'height': '100%',
+        //         //     'top': '0%',
+        //         //     'bottom': '0%',
+        //         //     'background': 'inherit',
+        //         //     '-webkit-filter': filterVal,
+        //         //     '-moz-filter': filterVal,
+        //         //     'filter': filterVal,
+        //         // });
+
+                
+        //         // var block = (function() {
+        //         //     var node = document.createElement("div");
+        //         //     Object.assign(node.style, {
+        //         //         'position': 'absolute',
+        //         //         'display': 'block',
+        //         //         'overflow': 'hidden',
+        //         //         'visibility': 'visible',
+        //         //         'width': '400px',
+        //         //         'height': '200px',
+        //         //         'background': 'inherit',
+        //         //         'border-radius': '10px',
+        //         //     });
+        //         //     return blurParent.appendChild(node);
+        //         // })();
+
+        //         // var test = (function() {
+        //         //     var node = document.createElement("div");
+        //         //     Object.assign(node.style, {
+        //         //         'display': 'block',
+        //         //         'background-color': 'rgba(235,235,235,0.6)',
+        //         //     });
+        //         //     return block.appendChild(node);
+        //         // })();
+                
+        //         // var updateBg = function() {
+        //         //     domtoimage.toPng(document.body).then(function (dataUrl) {
+        //         //         Object.assign(blurParent.style, {
+        //         //             'background': 'url(' + dataUrl + ') no-repeat'
+        //         //         })
+        //         //     });
+        //         // };
+            
+        //         // updateBg();
+        //         // window.addEventListener("resize", updateBg);
+        //     });
+        // });
     });
 }
