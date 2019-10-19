3ba2e1784074241420aa4415acd732267c3517d1
mkrugr <m.a.kruger4@gmail.com>
2019-10-19 17:34:07 +0200

    new twig template for new registration

---
+++ src/Form/Activity/RegistrationType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form\Activity;
+
+use App\Entity\Activity\Registration;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityRepository;
+use App\Entity\Activity\PriceOption;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('person', EntityType::class, [
+                'label' => 'Naam',
+                'class' => 'App\Entity\Person\Person',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.firstname', 'ASC');
+                },
+                'choice_label' => function ($ref) {
+                    return $ref->getFirstname().' '.$ref->getLastname();
+                },
+            ])
+            ->add('option', EntityType::class, [
+                'label' => 'Optie',
+                'class' => 'App\Entity\Activity\PriceOption',
+                'choices' => $options['allowed_options'],
+                'choice_label' => function ($ref) {
+                    return $ref->getName().' '.number_format($ref->getPrice() / 100, 2, '.', '').'€';
+                },
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        //$resolver->setAllowedTypes('allowed_options', ['PriceOption', 'string', 'array']);
+
+        $resolver->setDefaults([
+            'data_class' => Registration::class,
+            'allowed_options' => null,
+        ]);
+    }
+}
