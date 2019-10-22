9547f5031b452cfb38648746372701ebaeeb2ab0
Jasper Weyne <jasperweyne@gmail.com>
2019-10-22 20:11:51 +0200

    Generate basic menu's for groups

---
+++ src/Group/GroupMenuExtension.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Group;
+
+use App\Entity\Group\Taxonomy;
+use App\Template\MenuExtensionInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class GroupMenuExtension implements MenuExtensionInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var array
+     */
+    private $menuItems = [];
+
+    /**
+     * GroupMenuExtension constructor.
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Returns all the menu items.
+     */
+    public function getMenuItems(string $menu = '')
+    {
+        if (!$this->menuItems) {
+            $this->discoverMenuItems();
+        }
+
+        if (!array_key_exists($menu, $this->menuItems)) {
+            return [];
+        }
+
+        return $this->menuItems[$menu];
+    }
+
+    /**
+     * Discovers menu items.
+     */
+    private function discoverMenuItems()
+    {
+        $groups = $this->em->getRepository(Taxonomy::class)->findBy(['category' => true]);
+
+        $mapped = [];
+        foreach ($groups as $group) {
+            $mapped[] = [
+                'title' => $group->getName(),
+                'path' => 'admin_group_index',
+            ];
+        }
+
+        $this->menuItems['admin'] = $mapped;
+    }
+}
