b0cb9189c77854499c4d098f3c93ef8d869800c5
Jasper Weyne <jasperweyne@gmail.com>
2019-11-18 20:29:36 +0100

    Basic pagination for events

--- src/Controller/Admin/EventController.php
+++ src/Controller/Admin/EventController.php
@@ -5,8 +5,10 @@ namespace App\Controller\Admin;
 use App\Template\Annotation\MenuItem;
 use App\Entity\Log\Event;
 use App\Log\EventService;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Event controller.
@@ -28,14 +30,54 @@ class EventController extends AbstractController
      * @MenuItem(title="Gebeurtenislog", menu="admin")
      * @Route("/", name="index", methods={"GET"})
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder()
+            ->select('e')
+            ->from(Event::class, 'e')
+            ->orderBy('e.time', 'DESC')
+        ;
 
-        $log = $em->getRepository(Event::class)->findBy([], ['time' => 'DESC']);
+        $pagination = $this->paginate($qb, $request->query->getInt('index'), $request->query->getInt('results', 30));
 
         return $this->render('admin/event/index.html.twig', [
-            'log' => $this->events->populateAll($log),
+            'log' => $this->events->populateAll($pagination['results']),
+            'pagination' => $pagination,
         ]);
+    }
+
+    private function paginate(QueryBuilder $qb, int $index = 0, int $limit = 30)
+    {
+        $index = \max(0, $index);
+        $limit = \max(1, $limit);
+
+        $cqb = clone $qb;
+        $count = current($cqb
+                    ->select('count('.$qb->getRootAlias().')')
+                    ->getQuery()
+                    ->getOneOrNullResult()
+                );
+
+        $rqb = clone $qb;
+        $results = $rqb
+            ->setFirstResult($index)
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult()
+        ;
+
+        $prev = $index > 0 ? \max(0, $index - $limit) : null;
+        $next = $index + $limit < $count ? $index + $limit : null;
+
+        return [
+            'total' => $count,
+            'results' => $results,
+            'hasPrev' => !\is_null($prev),
+            'hasNext' => !\is_null($next),
+            'prev' => $prev,
+            'index' => $index,
+            'next' => $next,
+        ];
     }
 }
