167219de1e2eaa82ff2d0907af2e40f140f5daa2
Jasper Weyne <jasperweyne@gmail.com>
2020-01-15 17:28:59 +0100

    Add activity management actions

--- src/Controller/Organise/OrganiseController.php
+++ src/Controller/Organise/OrganiseController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Organise;
 
 use App\Entity\Activity\Activity;
-use App\Entity\Activity\Registration;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Group\Group;
@@ -18,30 +17,13 @@ class OrganiseController extends AbstractController
     /**
      * Lists all activities.
      *
-     * @Route("/activity/{id}", name="activity", methods={"GET"})
-     */
-    public function activityAction(Activity $activity)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $deregs = $em->getRepository(Registration::class)->findDeregistrations($activity);
-
-        return $this->render('organise/activity.html.twig', [
-            'activity' => $activity,
-            'deregistrations' => $deregs,
-        ]);
-    }
-
-    /**
-     * Lists all activities.
-     *
      * @Route("/{id}", name="index", methods={"GET"})
      */
     public function indexAction(Group $group)
     {
         $em = $this->getDoctrine()->getManager();
 
-        $activities = $em->getRepository(Activity::class)->findAll(); //findBy(['author' => $group]);
+        $activities = $em->getRepository(Activity::class)->findBy(['author' => $group]);
 
         return $this->render('organise/index.html.twig', [
             'group' => $group,
---
+++ src/Form/Activity/Admin/ActivityEditType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Form\Activity\Admin;
+
+use App\Form\Location\LocationType;
+use App\Entity\Activity\Activity;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ActivityEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description', TextareaType::class)
+            ->add('location', LocationType::class)
+            ->add('deadline', DateTimeType::class, [
+                'date_widget' => 'single_text',
+                'time_widget' => 'single_text',
+            ])
+            ->add('author', EntityType::class, [
+                'label' => 'Georganiseerd door',
+                'class' => 'App\Entity\Group\Group',
+                'required' => false,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('t')
+                        ->andWhere('t.active = TRUE');
+                },
+                'choice_label' => function ($ref) {
+                    return $ref->getName();
+                },
+            ])
+            ->add('start', DateTimeType::class, [
+                'date_widget' => 'single_text',
+                'time_widget' => 'single_text',
+            ])
+            ->add('end', DateTimeType::class, [
+                'date_widget' => 'single_text',
+                'time_widget' => 'single_text',
+            ])
+            ->add('color', ChoiceType::class, [
+                'attr' => ['data-select' => 'true'],
+                'choices' => [
+                    '' => null,
+                    'Rood' => 'red',
+                    'Oranje' => 'orange',
+                    'Geel' => 'yellow',
+                    'Groen' => 'green',
+                    'Cyaan' => 'cyan',
+                    'Lichtblauw' => 'ltblue',
+                    'Blauw' => 'blue',
+                    'Paars' => 'purple',
+                    'Roze' => 'pink',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Activity::class,
+        ]);
+    }
+}
