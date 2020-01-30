65c3de52e2336f546d9324989cb32f128d2dfcee
Jasper Weyne <jasperweyne@gmail.com>
2020-01-30 18:32:45 +0100

    User Person data edit form

--- src/Controller/Profile/ProfileController.php
+++ src/Controller/Profile/ProfileController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\Profile;
 
+use App\Security\AuthUserProvider;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Activity controller.
@@ -23,6 +25,34 @@ class ProfileController extends AbstractController
 
         return $this->render('profile/index.html.twig', [
             'personal' => $personal,
+        ]);
+    }
+
+    /**
+     * Displays a form to edit logged in person.
+     *
+     * @Route("/edit", name="edit", methods={"GET", "POST"})
+     */
+    public function editAction(Request $request, AuthUserProvider $authProvider)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $person = $this->getUser()->getPerson();
+
+        $form = $this->createForm('App\Form\Person\PersonType', $person, ['person' => $person]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $auth = $person->getAuth();
+            $auth->setAuthId($authProvider->usernameHash($person->getEmail()));
+
+            $em->flush();
+
+            return $this->redirectToRoute('profile_index');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'person' => $person,
+            'form' => $form->createView(),
         ]);
     }
 }
