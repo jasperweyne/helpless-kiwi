0759adacc167de93f180b5aef91715cce7d15d8d
Jasper Weyne <jasperweyne@gmail.com>
2020-04-11 00:05:52 +0200

    Split Organise/ActivityController to RegistrationController

---
+++ src/Controller/Organise/RegistrationController.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace App\Controller\Organise;
+
+use App\Entity\Activity\Activity;
+use App\Entity\Activity\Order;
+use App\Entity\Activity\Registration;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Activity\PriceOption;
+use App\Entity\Group\Group;
+use App\Mail\MailService;
+
+/**
+ * Activity controller.
+ *
+ * @Route("/organise/activity/register", name="organise_activity_registration_")
+ */
+class RegistrationController extends ActivityController
+{
+    /**
+     * Displays a form to edit an existing activity entity.
+     *
+     * @Route("/new/{id}", name="new", methods={"GET", "POST"})
+     */
+    public function registrationNewAction(Request $request, Activity $activity, MailService $mailer)
+    {
+        $this->blockUnauthorisedUsers($activity->getAuthor());
+
+        $em = $this->getDoctrine()->getManager();
+
+        $registration = new Registration();
+        $registration->setActivity($activity);
+
+        $now = new \DateTime('now');
+        $registration->setNewDate($now);
+
+        $form = $this->createForm('App\Form\Activity\RegistrationType', $registration, [
+            'allowed_options' => $activity->getOptions(),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($registration);
+            $em->flush();
+
+            $this->addFlash('success', $registration->getPerson()->getCanonical().' aangemeld!');
+
+            $title = 'Aanmeldbericht '.$registration->getActivity()->getName();
+            $body = $this->renderView('email/newregistration_by.html.twig', [
+                'person' => $this->getUser()->getPerson(),
+                'activity' => $registration->getActivity(),
+                'title' => $title,
+                'by' => $this->getUser()->getPerson(),
+            ]);
+
+            $mailer->message($this->getUser()->getPerson(), $title, $body);
+
+            return $this->redirectToRoute('organise_activity_show', ['id' => $activity->getId()]);
+        }
+
+        return $this->render('organise/activity/registration/new.html.twig', [
+            'activity' => $activity,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Deletes a person entity.
+     *
+     * @Route("/delete/{id}", name="registration_delete")
+     */
+    public function registrationDeleteAction(Request $request, Registration $registration, MailService $mailer)
+    {
+        $this->blockUnauthorisedUsers($registration->getActivity()->getAuthor());
+
+        $form = $this->createRegistrationDeleteForm($registration);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $now = new \DateTime('now');
+            $registration->setDeleteDate($now);
+
+            $em->flush();
+
+            $this->addFlash('success', $registration->getPerson()->getCanonical().' afgemeld!');
+
+            $title = 'Afmeldbericht '.$registration->getActivity()->getName();
+            $body = $this->renderView('email/removedregistration_by.html.twig', [
+                'person' => $this->getUser()->getPerson(),
+                'activity' => $registration->getActivity(),
+                'title' => $title,
+                'by' => $this->getUser()->getPerson(),
+            ]);
+
+            $mailer->message($this->getUser()->getPerson(), $title, $body);
+
+            return $this->redirectToRoute('organise_activity_show', ['id' => $registration->getActivity()->getId()]);
+        }
+
+        return $this->render('organise/activity/registration/delete.html.twig', [
+            'registration' => $registration,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+
+    /**
+     * Displays a form to edit an existing activity entity.
+     *
+     * @Route("/reserve/new/{id}", name="reserve_new", methods={"GET", "POST"})
+     */
+    public function reserveNewAction(Request $request, Activity $activity, MailService $mailer)
+    {
+        $this->blockUnauthorisedUsers($activity->getAuthor());
+
+        $em = $this->getDoctrine()->getManager();
+        
+        $position = $em->getRepository(Registration::class)->findAppendPosition($activity);
+
+        $registration = new Registration();
+        $registration
+            ->setActivity($activity)
+            ->setReservePosition($position)
+        ;
+
+        $now = new \DateTime('now');
+        $registration->setNewDate($now);
+
+        $form = $this->createForm('App\Form\Activity\RegistrationType', $registration, [
+            'allowed_options' => $activity->getOptions(),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($registration);
+            $em->flush();
+
+            $this->addFlash('success', $registration->getPerson()->getCanonical().' aangemeld op de reservelijst!');
+
+            return $this->redirectToRoute('organise_activity_show', ['id' => $activity->getId()]);
+        }
+
+        return $this->render('organise/activity/registration/new.html.twig', [
+            'activity' => $activity,
+            'form' => $form->createView(),
+            'reserve' => true,
+        ]);
+    }
+
+    /**
+     * Displays a form to edit an existing activity entity.
+     *
+     * @Route("/reserve/move/{id}/up", name="reserve_move_up", methods={"GET", "POST"})
+     */
+    public function reserveMoveUpAction(Request $request, Registration $registration)
+    {
+        $this->blockUnauthorisedUsers($registration->getActivity()->getAuthor());
+
+        $em = $this->getDoctrine()->getManager();
+
+        $x1 = $em->getRepository(Registration::class)->findBefore($registration->getActivity(), $registration->getReservePosition());
+        $x2 = $em->getRepository(Registration::class)->findBefore($registration->getActivity(), $x1);
+
+        $registration->setReservePosition(Order::avg($x1, $x2));
+
+        $em->flush();
+
+        $this->addFlash('success', $registration->getPerson()->getCanonical().' naar boven verplaatst!');
+
+        return $this->redirectToRoute('organise_activity_show', ['id' => $registration->getActivity()->getId()]);
+    }
+
+    /**
+     * Displays a form to edit an existing activity entity.
+     *
+     * @Route("/reserve/move/{id}/down", name="reserve_move_down", methods={"GET", "POST"})
+     */
+    public function reserveMoveDownAction(Request $request, Registration $registration)
+    {
+        $this->blockUnauthorisedUsers($registration->getActivity()->getAuthor());
+
+        $em = $this->getDoctrine()->getManager();
+
+        $x1 = $em->getRepository(Registration::class)->findAfter($registration->getActivity(), $registration->getReservePosition());
+        $x2 = $em->getRepository(Registration::class)->findAfter($registration->getActivity(), $x1);
+
+        $registration->setReservePosition(Order::avg($x1, $x2));
+
+        $em->flush();
+
+        $this->addFlash('success', $registration->getPerson()->getCanonical().' naar beneden verplaatst!');
+
+        return $this->redirectToRoute('organise_activity_show', ['id' => $registration->getActivity()->getId()]);
+    }
+
+    /**
+     * Creates a form to check out all checked in users.
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createRegistrationDeleteForm(Registration $registration)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('organise_activity_registration_delete', ['id' => $registration->getId()]))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
