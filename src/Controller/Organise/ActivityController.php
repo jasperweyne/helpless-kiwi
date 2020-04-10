24b436fe211e9fccf618d448ebcc782cddd06d83
Jasper Weyne <jasperweyne@gmail.com>
2020-04-10 22:50:19 +0200

    Add reserve functionality to organise

--- src/Controller/Organise/ActivityController.php
+++ src/Controller/Organise/ActivityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Organise;
 
 use App\Entity\Activity\Activity;
+use App\Entity\Activity\Order;
 use App\Entity\Activity\Registration;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -77,11 +78,17 @@ class ActivityController extends AbstractController
 
         $em = $this->getDoctrine()->getManager();
 
-        $deregs = $em->getRepository(Registration::class)->findDeregistrations($activity);
+        $repository = $em->getRepository(Registration::class);
+
+        $regs = $repository->findBy(['activity' => $activity, 'deletedate' => null, 'reserve_position' => null]);
+        $deregs = $repository->findDeregistrations($activity);
+        $reserve = $repository->findReserve($activity);
 
         return $this->render('organise/activity/show.html.twig', [
             'activity' => $activity,
+            'registrations' => $regs,
             'deregistrations' => $deregs,
+            'reserve' => $reserve,
         ]);
     }
 
@@ -314,6 +321,98 @@ class ActivityController extends AbstractController
             'registration' => $registration,
             'form' => $form->createView(),
         ]);
+    }
+
+
+
+    /**
+     * Displays a form to edit an existing activity entity.
+     *
+     * @Route("/reserve/new/{id}", name="reserve_new", methods={"GET", "POST"})
+     */
+    public function reserveNewAction(Request $request, Activity $activity, MailService $mailer)
+    {
+        $this->blockUnauthorisedUsers($activity->getAuthor());
+
+        $em = $this->getDoctrine()->getManager();
+        
+        $position = $em->getRepository(Registration::class)->findAppendPosition($activity);
+
+        $registration = new Registration();
+        $registration
+            ->setActivity($activity)
+            ->setReservePosition($position)
+        ;
+
+        $now = new \DateTime('now');
+        $registration->setNewDate($now);
+
+        $form = $this->createForm('App\Form\Activity\RegistrationType', $registration, [
+            'allowed_options' => $activity->getOptions(),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($registration);
+            $em->flush();
+
+            $this->addFlash('success', $registration->getPerson()->getCanonical().' aangemeld op de reservelijst!');
+
+            return $this->redirectToRoute('admin_activity_show', ['id' => $activity->getId()]);
+        }
+
+        return $this->render('admin/activity/registration/new.html.twig', [
+            'activity' => $activity,
+            'form' => $form->createView(),
+            'reserve' => true,
+        ]);
+    }
+
+    /**
+     * Displays a form to edit an existing activity entity.
+     *
+     * @Route("/reserve/move/{id}/up", name="reserve_move_up", methods={"GET", "POST"})
+     */
+    public function reserveMoveUpAction(Request $request, Registration $registration)
+    {
+        $this->blockUnauthorisedUsers($registration->getActivity()->getAuthor());
+
+        $em = $this->getDoctrine()->getManager();
+
+        $x1 = $em->getRepository(Registration::class)->findBefore($registration->getActivity(), $registration->getReservePosition());
+        $x2 = $em->getRepository(Registration::class)->findBefore($registration->getActivity(), $x1);
+
+        $registration->setReservePosition(Order::avg($x1, $x2));
+
+        $em->flush();
+
+        $this->addFlash('success', $registration->getPerson()->getCanonical().' naar boven verplaatst!');
+
+        return $this->redirectToRoute('admin_activity_show', ['id' => $registration->getActivity()->getId()]);
+    }
+
+    /**
+     * Displays a form to edit an existing activity entity.
+     *
+     * @Route("/reserve/move/{id}/down", name="reserve_move_down", methods={"GET", "POST"})
+     */
+    public function reserveMoveDownAction(Request $request, Registration $registration)
+    {
+        $this->blockUnauthorisedUsers($registration->getActivity()->getAuthor());
+
+        $em = $this->getDoctrine()->getManager();
+
+        $x1 = $em->getRepository(Registration::class)->findAfter($registration->getActivity(), $registration->getReservePosition());
+        $x2 = $em->getRepository(Registration::class)->findAfter($registration->getActivity(), $x1);
+
+        $registration->setReservePosition(Order::avg($x1, $x2));
+
+        $em->flush();
+
+        $this->addFlash('success', $registration->getPerson()->getCanonical().' naar beneden verplaatst!');
+
+        return $this->redirectToRoute('admin_activity_show', ['id' => $registration->getActivity()->getId()]);
     }
 
     /**
