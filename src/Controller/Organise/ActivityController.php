f0739e310b2aef304350318e6e7673a2da49b851
David <davidc.koymans@outlook.com>
2020-11-02 19:07:26 +0100

    Added the same functionality to the Organisation ActivityController

--- src/Controller/Organise/ActivityController.php
+++ src/Controller/Organise/ActivityController.php
@@ -208,13 +208,18 @@ class ActivityController extends AbstractController
     public function priceEditAction(Request $request, PriceOption $price)
     {
         $this->blockUnauthorisedUsers($price->getActivity()->getAuthor());
-
+        
+        $activity = $price->getActivity();
         $originalPrice = $price->getPrice();
         $form = $this->createForm('App\Form\Activity\PriceOptionType', $price);
         $form->handleRequest($request);
 
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository(Registration::class);
+
         if ($form->isSubmitted() && $form->isValid()) {
-            if (count($price->getRegistrations()) > 0 && $originalPrice < $price->getPrice()) {
+            $regs = $repository->findBy(['activity' => $activity, 'deletedate' => null, 'reserve_position' => null]);
+            if (count($regs) > 0 && $originalPrice < $price->getPrice()) {
                 $this->addFlash('error', 'Prijs kan niet verhoogd worden als er al deelnemers geregistreerd zijn');
 
                 return $this->render('organise/activity/price/edit.html.twig', [
