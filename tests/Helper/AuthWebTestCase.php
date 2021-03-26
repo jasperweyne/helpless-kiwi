1ba519a74a9d8ce551455a694d2278a25bcd0d49
Peter Sabel (Zabel) <peter@sabelzabel.nl>
2021-03-26 01:40:19 +0100

    fix(AuthWebTestCase): implemented Logout feature
    Semi late night with Utrecht, now we can also log out during testing.
    Sortof important I guess.

--- tests/Helper/AuthWebTestCase.php
+++ tests/Helper/AuthWebTestCase.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Helper;
 
-use Tests\Helper\Database\Security\LocalAccountFixture;
 use App\Entity\Security\LocalAccount;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\SchemaTool;
@@ -10,6 +9,7 @@ use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
+use Tests\Helper\Database\Security\LocalAccountFixture;
 
 /**
  * Extends the WebTestCase class with support for logging in and fixtures.
@@ -32,7 +32,7 @@ class AuthWebTestCase extends WebTestCase
 
         $this->client = static::createClient();
         $this->client->followRedirects(true);
-        
+
         // Get all database tables
         $em = self::$container->get(EntityManagerInterface::class);
         $cmf = $em->getMetadataFactory();
@@ -72,5 +72,11 @@ class AuthWebTestCase extends WebTestCase
 
         $cookie = new Cookie($session->getName(), $session->getId());
         $this->client->getCookieJar()->set($cookie);
+    }
+
+    protected function logout(): void
+    {
+        $this->client->getContainer()->get('session')->invalidate();
+        $this->client->getContainer()->get('security.token_storage')->setToken(null);
     }
 }
