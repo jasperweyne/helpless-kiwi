9882aa07f29577112a8768abeca315af1323a7c2
David <davidc.koymans@outlook.com>
2021-11-22 20:25:03 +0100

    small mistake which caused server to crash,

--- src/Entity/Group/Relation.php
+++ src/Entity/Group/Relation.php
@@ -6,6 +6,7 @@ use App\Entity\Security\LocalAccount;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Overblog\GraphQLBundle\Annotation as GQL;
 
 /**
  * @ORM\Entity
