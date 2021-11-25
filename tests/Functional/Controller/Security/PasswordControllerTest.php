f4db93c7fc6dfba37a4ac58a5b5ae4f6ab9fc825
Peter <peter@sabelzabel.nl>
2021-11-25 15:42:30 +0100

    fix(PasswordController): enpoint again.
    yes, writing is hard, to test register you cant
    call request

--- tests/Functional/Controller/Security/PasswordControllerTest.php
+++ tests/Functional/Controller/Security/PasswordControllerTest.php
@@ -135,7 +135,7 @@ class PasswordControllerTest extends AuthWebTestCase
         $auth = $this->userProvider->loadUserByUsername(LocalAccountFixture::USERNAME);
         $auth->setPasswordRequestedAt(new \DateTime());
         $this->passwordReset->generatePasswordRequestToken($auth);
-        $this->client->request('GET', '/password/request/'.$auth->getId().'?token='.urlencode("invalid-token"));
+        $this->client->request('GET', '/password/register/'.$auth->getId().'?token='.urlencode("invalid-token"));
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertSelectorTextContains('.container', 'Invalid password token.');
     }
