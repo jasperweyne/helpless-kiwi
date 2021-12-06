b0e6a4a3b0e293d7e246ab76e5fb93910f9154ff
Jasper Weyne <jasperweyne@gmail.com>
2021-12-06 23:20:34 +0100

    Fix broken migration
    This commit had some duplicate and therefore broken migration behaviour.
    The duplicate behaviour is removed.

--- migrations/Version20210624181858.php
+++ migrations/Version20210624181858.php
@@ -14,7 +14,7 @@ final class Version20210624181858 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add comments to database';
     }
 
     public function up(Schema $schema): void
@@ -22,8 +22,6 @@ final class Version20210624181858 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE kiwi_oauth2access_token');
-        $this->addSql('ALTER TABLE kiwi_activity DROP hidden');
         $this->addSql('ALTER TABLE kiwi_registration ADD comment VARCHAR(255) DEFAULT NULL');
     }
 
@@ -32,8 +30,6 @@ final class Version20210624181858 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE kiwi_oauth2access_token (id VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, access_token JSON NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE kiwi_activity ADD hidden TINYINT(1) DEFAULT NULL');
         $this->addSql('ALTER TABLE kiwi_registration DROP comment');
     }
 }
