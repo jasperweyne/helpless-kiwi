df6894cebb339cca9759c4ee745dee5d9edbd5d3
Jasper Weyne <jasperweyne@gmail.com>
2021-12-06 21:27:39 +0100

    Fix broken migration for hidden activities
    This commit removes an invalid migration and replaces it with a migration
    which adds the hidden marker to activities.

--- migrations/Version20211206202433.php
+++ migrations/Version20211206202433.php
@@ -10,11 +10,11 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210624181610 extends AbstractMigration
+final class Version20211206202433 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add hidden activities';
     }
 
     public function up(Schema $schema): void
@@ -22,7 +22,7 @@ final class Version20210624181610 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE kiwi_oauth2access_token');
+        $this->addSql('ALTER TABLE kiwi_activity ADD hidden TINYINT(1) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
@@ -30,6 +30,6 @@ final class Version20210624181610 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE kiwi_oauth2access_token (id VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, access_token JSON NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE kiwi_activity DROP hidden');
     }
 }
