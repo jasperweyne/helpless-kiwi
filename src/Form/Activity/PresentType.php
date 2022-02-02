213ef1880cda53c2efc48042e68664a2f462e7ed
David <71597250+DavidckPixel@users.noreply.github.com>
2022-02-02 22:24:59 +0100

    Modified the way Present Types are set to be more usefull.
    PresentType.php (form) -> changed the ChoiceType class to include expended => true, multiple => false
    
    This should make it easier for admins to set present

--- src/Form/Activity/PresentType.php
+++ src/Form/Activity/PresentType.php
@@ -27,12 +27,14 @@ class PresentType extends AbstractType
             $builder
                 ->add('present', ChoiceType::class, [
                     'choices' => [
-                        'Onbekend' => null,
-                        'Aanwezig' => true,
-                        'Afwezig' => false,
+                        ' Onbekend' => null,
+                        ' Aanwezig' => true,
+                        ' Afwezig' => false,
                     ],
                     'label' => $registration->getPerson(),
                     'required' => true,
+                    'multiple' => false,
+                    'expanded' => true,
                 ]);
         });
     }
