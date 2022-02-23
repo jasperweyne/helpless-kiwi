1b233810cdaf5b5139f48c00bff6e379b2b800bd
Peter <peter@sabelzabel.nl>
2022-02-23 12:54:40 +0100

    fix: logincontroller removed unreachable exception

--- src/Controller/Security/LoginController.php
+++ src/Controller/Security/LoginController.php
@@ -58,6 +58,5 @@ class LoginController extends AbstractController
     public function logout()
     {
         // controller can be blank: it will never be executed!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
