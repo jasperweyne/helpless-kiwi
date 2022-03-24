35e5ad79adc441faa21b6794620fa0a47287907d
Jasper Weyne <jasperweyne@gmail.com>
2022-03-24 14:17:46 +0100

    Improve integration between phpstan and kiwi
    This commit adds additional integration between Kiwi and Phpstan. This
    adds a number of new (higher quality) detected issues, and removes some
    lower tier issues as well. Two files in the config are added that allow
    for the integration, and the baseline file has been updated with the new issues as well.

---
+++ config/object-manager.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Kernel;
+
+require __DIR__.'/bootstrap.php';
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
