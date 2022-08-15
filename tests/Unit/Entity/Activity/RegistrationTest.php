ba1d1098c9593ab95729cfa803e13bb92b6e2ac3
Peter <peter@sabelzabel.nl>
2022-08-15 22:44:19 +0200

    test: Registration setPerson as external

--- tests/Unit/Entity/Activity/RegistrationTest.php
+++ tests/Unit/Entity/Activity/RegistrationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Entity\Activity;
 
 use App\Entity\Activity\Activity;
+use App\Entity\Activity\ExternalRegistrant;
 use App\Entity\Activity\PriceOption;
 use App\Entity\Activity\Registration;
 use App\Entity\Order;
@@ -103,6 +104,17 @@ class RegistrationTest extends KernelTestCase
         $expected->setEmail('john@doe.eyes');
         $property = (new ReflectionClass(Registration::class))
             ->getProperty('person');
+        $property->setAccessible(true);
+        $this->registration->setPerson($expected);
+        self::assertSame($expected, $this->registration->getPerson());
+    }
+
+    public function testSetPersonExternal(): void
+    {
+        $expected = new LocalAccount();
+        $expected->setEmail('john@doe.eyes');
+        $property = (new ReflectionClass(ExternalRegistrant::class))
+            ->getProperty('name');
         $property->setAccessible(true);
         $this->registration->setPerson($expected);
         self::assertSame($expected, $this->registration->getPerson());
