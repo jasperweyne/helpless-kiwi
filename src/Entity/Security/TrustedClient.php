e1241ed1279e25d2e5475e1bc8339ca16080af91
Jasper Weyne <jasperweyne@gmail.com>
2022-12-29 14:24:18 +0100

    Add OneToMany on TrustedClient to ApiToken

--- src/Entity/Security/TrustedClient.php
+++ src/Entity/Security/TrustedClient.php
@@ -2,17 +2,26 @@
 
 namespace App\Entity\Security;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 class TrustedClient
 {
+    /**
+     * @var Collection<int, ApiToken>
+     */
+    #[ORM\OneToMany(targetEntity: ApiToken::class, mappedBy: 'client')]
+    public readonly Collection $tokens;
+
     public function __construct(
         #[ORM\Id]
         #[ORM\Column(length: 255)]
         public readonly string $id,
         #[ORM\Column(length: 255)]
-        public readonly string $secret,
+        public readonly string $secret
     ) {
+        $this->tokens = new ArrayCollection();
     }
 }
