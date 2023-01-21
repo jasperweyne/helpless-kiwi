5196563fcb38f4f1602ff096b23899f723ef7fb6
Jasper Weyne <jasperweyne@gmail.com>
2023-01-21 20:18:26 +0100

    TrustedClientController initial functional tests

--- tests/Functional/Controller/Admin/TrustedClientControllerTest.php
+++ tests/Functional/Controller/Admin/TrustedClientControllerTest.php
@@ -2,34 +2,28 @@
 
 namespace Tests\Functional\Controller\Admin;
 
-use App\Controller\Admin\SecurityController;
-use App\Entity\Security\LocalAccount;
-use App\Log\EventService;
+use App\Entity\Security\TrustedClient;
 use App\Tests\AuthWebTestCase;
 use App\Tests\Database\Security\LocalAccountFixture;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DomCrawler\Field\FormField;
 
 /**
- * Class SecurityControllerTest.
+ * Class TrustedClientControllerTest.
  *
- * @covers \App\Controller\Admin\SecurityController
+ * @covers \App\Controller\Admin\TrustedClientController
  */
 class TrustedClientControllerTest extends AuthWebTestCase
 {
     /**
-     * @var SecurityController
-     */
-    protected $securityController;
-
-    /**
-     * @var EventService
-     */
-    protected $events;
-
-    /**
      * @var EntityManagerInterface
      */
     protected $em;
+
+    /**
+     * @var string
+     */
+    private $controllerEndpoint = '/admin/security/client';
 
     /**
      * {@inheritdoc}
@@ -56,56 +50,61 @@ class TrustedClientControllerTest extends AuthWebTestCase
         unset($this->em);
     }
 
-    public function testGetMenuItems(): void
-    {
-        /* @todo This test is incomplete. */
-        self::markTestIncomplete();
-    }
-
     public function testIndexAction(): void
     {
-        // Act
-        $this->client->request('GET', '/admin/security/');
-
-        // Assert
+        $this->client->request('GET', $this->controllerEndpoint.'/');
+        self::assertSelectorTextContains('span', 'API');
         self::assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testNewAction(): void
+    public function testIndexActionNotAdmin(): void
     {
-        // Act
-        $crawler = $this->client->request('GET', '/admin/security/new');
-        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
-        $form = $crawler->selectButton('Toevoegen')->form();
-        $form['local_account[name]'] = 'John';
-        $form['local_account[email]'] = 'john@doe.eyes';
-        $crawler = $this->client->submit($form);
-
-        // Assert
-        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
-        // TODO: figure our if this is actually the way to test this....
-        self::assertEquals(1, $crawler->filter('.top > h3:nth-child(1)')->count());
+        $this->logout();
+        $this->login([]);
+        $this->client->request('GET', $this->controllerEndpoint.'/');
+        self::assertEquals(403, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testShowAction(): void
+    public function testNewActionGet(): void
+    {
+        $this->client->request('GET', $this->controllerEndpoint . "/new");
+        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     *  @depends testNewActionGet
+     */
+    public function testNewActionPost(): void
+    {
+        // Arrange
+        $originalCount = $this->em->getRepository(TrustedClient::class)->count([]);
+
+        // Act
+        $crawler = $this->client->request('GET', $this->controllerEndpoint . "/new");
+        $form = $crawler->selectButton('Toevoegen')->form();
+        ($field = $form['form[id]']) instanceof FormField && $field->setValue('Test');
+        $this->client->submit($form);
+
+        // Assert
+        $newCount = $this->em->getRepository(TrustedClient::class)->count([]);
+        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
+        self::assertSelectorExists('.messages .flash-success');
+        self::assertEquals(1, $newCount - $originalCount, "TrustedClient count didn't correctly change after POST request.");
+    }
+
+    public function testClearAction(): void
     {
         /* @todo This test is incomplete. */
         self::markTestIncomplete();
     }
 
-    public function testEditAction(): void
+    public function testTokenAction(): void
     {
         /* @todo This test is incomplete. */
         self::markTestIncomplete();
     }
 
     public function testDeleteAction(): void
-    {
-        /* @todo This test is incomplete. */
-        self::markTestIncomplete();
-    }
-
-    public function testRolesAction(): void
     {
         /* @todo This test is incomplete. */
         self::markTestIncomplete();
