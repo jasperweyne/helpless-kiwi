43ffe3b4fb215e0a2388f65d63d4d93afd59f313
Jasper Weyne <jasperweyne@gmail.com>
2023-02-17 17:33:26 +0100

    Add unit test for MenuItem

--- tests/Unit/Template/Attribute/MenuItemTest.php
+++ tests/Unit/Template/Attribute/MenuItemTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Unit\Template\Attribute;
 
 use App\Template\Attribute\MenuItem;
-use ReflectionClass;
+use App\Template\Attribute\SubmenuItem;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 /**
@@ -13,36 +13,59 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
  */
 class MenuItemTest extends KernelTestCase
 {
-    /**
-     * @var MenuItem
-     */
-    protected $menuItem;
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-        self::bootKernel();
-
-        /* @todo Correctly instantiate tested object to use it. */
-        $this->menuItem = new MenuItem('test');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        unset($this->menuItem);
-    }
-
     public function testToMenuItemArray(): void
     {
-        /* @todo This test is incomplete. */
-        self::markTestIncomplete();
+        // Arrange
+        $title = 'title';
+        $menuItem = new MenuItem($title);
+
+        // Act
+        $result = $menuItem->toMenuItemArray();
+
+        // Assert
+        self::assertArrayNotHasKey('role', $result);
+        self::assertArrayNotHasKey('class', $result);
+        self::assertArrayNotHasKey('activeCriteria', $result);
+        self::assertArrayNotHasKey('order', $result);
+        self::assertArrayNotHasKey('sub', $result);
+        self::assertArrayHasKey('path', $result);
+
+        self::assertSame($title, $result['title']);
+        self::assertSame('', $result['path']);
+    }
+
+    public function testToMenuItemArrayWithValues(): void
+    {
+        // Arrange
+        $title = 'title';
+        $menu = 'menu';
+        $role = 'ROLE_ADMIN';
+        $class = 'mobile';
+        $activeCriteria = 'admin_title_';
+        $order = -1;
+        $sub = [
+            new SubmenuItem('test', 'admin_title_sub'),
+        ];
+        $path = 'admin_title_index';
+        $menuItem = new MenuItem($title, $menu, $role, $class, $activeCriteria, $order, $sub, $path);
+
+        // Act
+        $result = $menuItem->toMenuItemArray();
+
+        // Assert
+        self::assertArrayHasKey('role', $result);
+        self::assertArrayHasKey('class', $result);
+        self::assertArrayHasKey('activeCriteria', $result);
+        self::assertArrayHasKey('order', $result);
+        self::assertArrayHasKey('sub', $result);
+        self::assertArrayHasKey('path', $result);
+
+        self::assertSame($title, $result['title']);
+        self::assertSame($role, $result['role']);
+        self::assertSame($class, $result['class']);
+        self::assertSame($activeCriteria, $result['activeCriteria']);
+        self::assertSame($order, $result['order']);
+        self::assertSame($path, $result['path']);
+        self::assertCount(count($sub), $result['sub']);
     }
 }
