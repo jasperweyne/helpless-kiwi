f712d559e9e3c0515c5e53cc8a0c3e6c037ae995
Peter <peter@sabelzabel.nl>
2023-06-14 01:25:28 +0200

    test: MailNotificationSubscriber

--- tests/Unit/EventSubscriber/MailNotificationSubscriberTest.php
+++ tests/Unit/EventSubscriber/MailNotificationSubscriberTest.php
@@ -5,16 +5,17 @@ namespace Tests\Unit\EventSubscriber;
 use App\Calendar\ICalProvider;
 use App\Entity\Activity\Activity;
 use App\Entity\Activity\Registration;
+use App\Entity\Security\LocalAccount;
 use App\Event\RegistrationAddedEvent;
 use App\Event\RegistrationRemovedEvent;
+use App\Event\Security\CreateAccountsEvent;
 use App\EventSubscriber\MailNotificationSubscriber;
 use App\Mail\MailService;
-use App\Entity\Security\LocalAccount;
 use App\Security\PasswordResetService;
 use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Security\Core\Security;
 use Twig\Environment;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 /**
  * Class MailNotificationSubscriberTest.
@@ -23,35 +24,13 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
  */
 final class MailNotificationSubscriberTest extends KernelTestCase
 {
-    /**
-     * @var MailNotificationSubscriber
-     */
-    private $mailNotificationSubscriber;
-
-    /**
-     * @var Environment&MockObject
-     */
-    private $template;
-
-    /**
-     * @var MailService&MockObject
-     */
-    private $mailer;
-
-    /**
-     * @var ICalProvider&MockObject
-     */
-    private $calendar;
-
-    /**
-     * @var PasswordResetService&MockObject
-     */
-    private $passwordReset;
-
-    /**
-     * @var Security&MockObject
-     */
-    private $security;
+    private MailNotificationSubscriber $mailNotificationSubscriber;
+    private Environment&MockObject $template;
+    private MailService&MockObject $mailer;
+    private PasswordResetService&MockObject $passwordReset;
+    private LocalAccount&MockObject $localAccount;
+    private Security&MockObject $security;
+    private Registration&MockObject $registration;
 
     /**
      * {@inheritdoc}
@@ -60,18 +39,17 @@ final class MailNotificationSubscriberTest extends KernelTestCase
     {
         parent::setUp();
 
-        $this->template = $this->createMock(Environment::class);
-        $this->mailer = $this->createMock(MailService::class);
-        $this->calendar = $this->createMock(ICalProvider::class);
-        $this->security = $this->createMock(Security::class);
-        $this->passwordReset = $this->createMock(PasswordResetService::class);
-        $user = $this->createMock(LocalAccount::class);
-        $this->security->method('getUser')->will($this::returnValue($user));
+        $this->template = self::createMock(Environment::class);
+        $this->mailer = self::createMock(MailService::class);
+        $this->security = self::createMock(Security::class);
+        $this->passwordReset = self::createMock(PasswordResetService::class);
+        $this->localAccount = self::createMock(LocalAccount::class);
+        $this->registration = self::createMock(Registration::class);
 
         $this->mailNotificationSubscriber = new MailNotificationSubscriber(
             $this->template,
             $this->mailer,
-            $this->calendar,
+            self::createMock(ICalProvider::class),
             $this->passwordReset,
             $this->security
         );
@@ -84,12 +62,14 @@ final class MailNotificationSubscriberTest extends KernelTestCase
     {
         parent::tearDown();
 
-        unset($this->mailNotificationSubscriber);
         unset($this->template);
         unset($this->mailer);
-        unset($this->calendar);
-        unset($this->passwordReset);
         unset($this->security);
+        unset($this->passwordReset);
+        unset($this->localAccount);
+        unset($this->registration);
+
+        unset($this->mailNotificationSubscriber);
     }
 
     public function testGetSubscribedEvents(): void
@@ -111,57 +91,110 @@ final class MailNotificationSubscriberTest extends KernelTestCase
 
         // check if all methods exist
         foreach ($handlers as $handler) {
-            self::assertTrue(method_exists($this->mailNotificationSubscriber, $handler), "Method $handler does not exist");
+            self::assertTrue(
+                method_exists($this->mailNotificationSubscriber, $handler),
+                "Method $handler does not exist"
+            );
         }
     }
 
     public function testNotifyRegistrationAdded(): void
     {
-        /** @var Registration&MockObject */
-        $registration = $this->createMock(Registration::class);
-        $registration->method('getActivity')->willReturn($this->createMock(Activity::class));
+        $this->registration
+            ->method('getActivity')
+            ->willReturn(self::createMock(Activity::class));
 
         $this->template->method('render')->willReturn('<html>');
         $this->mailer->expects(self::once())->method('message');
 
-        $event = new RegistrationAddedEvent($registration);
+        $event = new RegistrationAddedEvent($this->registration);
         $this->mailNotificationSubscriber->notifyRegistrationAdded($event);
     }
 
     public function testNotifyRegistrationAddedReserve(): void
     {
-        /** @var Registration&MockObject */
-        $registration = $this->createMock(Registration::class);
-        $registration->method('isReserve')->willReturn(true);
+        $this->registration->method('isReserve')->willReturn(true);
 
         $this->mailer->expects(self::never())->method('message');
 
-        $event = new RegistrationAddedEvent($registration);
+        $event = new RegistrationAddedEvent($this->registration);
         $this->mailNotificationSubscriber->notifyRegistrationAdded($event);
     }
 
     public function testNotifyRegistrationRemoved(): void
     {
-        /** @var Registration&MockObject */
-        $registration = $this->createMock(Registration::class);
-        $registration->method('getActivity')->willReturn($this->createMock(Activity::class));
+        $this->registration
+            ->method('getActivity')
+            ->willReturn(self::createMock(Activity::class));
 
         $this->template->method('render')->willReturn('<html>');
         $this->mailer->expects(self::once())->method('message');
 
-        $event = new RegistrationRemovedEvent($registration);
+        $event = new RegistrationRemovedEvent($this->registration);
         $this->mailNotificationSubscriber->notifyRegistrationRemoved($event);
     }
 
     public function testNotifyRegistrationRemovedReserve(): void
     {
-        /** @var Registration&MockObject */
-        $registration = $this->createMock(Registration::class);
-        $registration->method('isReserve')->willReturn(true);
+        $this->registration->method('isReserve')->willReturn(true);
 
         $this->mailer->expects(self::never())->method('message');
 
-        $event = new RegistrationRemovedEvent($registration);
+        $event = new RegistrationRemovedEvent($this->registration);
         $this->mailNotificationSubscriber->notifyRegistrationRemoved($event);
+    }
+
+    public function testNotifyCreateAccount(): void
+    {
+        $event = new CreateAccountsEvent([$this->localAccount]);
+
+        $this->localAccount
+            ->expects($this::once())
+            ->method('getOidc')
+            ->willReturn(null);
+        $this->localAccount
+            ->expects($this::once())
+            ->method('getPassword')
+            ->willReturn(null);
+        $this->passwordReset
+            ->expects($this::once())
+            ->method('generatePasswordRequestToken')
+            ->with($this->localAccount, false)
+            ->willReturn('test_token');
+        $this->localAccount
+            ->expects($this::once())
+            ->method('setPasswordRequestedAt')
+            ->with(null);
+        $this->template
+            ->expects($this::once())
+            ->method('render')
+            ->willReturn('test_email_content');
+        $this->mailer
+            ->expects($this::once())
+            ->method('message')
+            ->with(
+                [$this->localAccount],
+                'Jouw account',
+                'test_email_content'
+            );
+
+        $this->mailNotificationSubscriber->notifyCreateAccount($event);
+    }
+
+    public function testNotifyCreateAccountWithLoginCredentials(): void
+    {
+        $this->localAccount
+            ->method('getOidc')
+            ->willReturn('mock_oidc');
+        $this->localAccount
+            ->method('getPassword')
+            ->willReturn('mock_password');
+        $event = new CreateAccountsEvent([$this->localAccount]);
+
+        $this->mailer
+            ->expects(self::never())
+            ->method('message');
+
+        $this->mailNotificationSubscriber->notifyCreateAccount($event);
     }
 }
