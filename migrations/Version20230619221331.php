6b7e56a3692c5ac90955006c811760e440019d02
Peter <peter@sabelzabel.nl>
2023-06-20 00:15:28 +0200

    fix: added migration for Entity alterations

---
+++ migrations/Version20230619221331.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230619221331 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE kiwi_activity CHANGE start start DATETIME DEFAULT NULL, CHANGE end end DATETIME DEFAULT NULL, CHANGE deadline deadline DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE kiwi_activity CHANGE start start DATETIME NOT NULL, CHANGE end end DATETIME NOT NULL, CHANGE deadline deadline DATETIME NOT NULL');
+    }
+}
