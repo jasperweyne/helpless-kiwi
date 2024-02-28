b1968e6a1b39a0f65c7dd06d24660d247a1a264b
Peter <peter@sabelzabel.nl>
2024-02-28 15:06:45 +0100

    feat: resolve #396
    Create account now require given and family name.
    Added a check that there is no existing account with the same email.

--- src/Form/Security/LocalAccountType.php
+++ src/Form/Security/LocalAccountType.php
@@ -3,19 +3,41 @@
 namespace App\Form\Security;
 
 use App\Entity\Security\LocalAccount;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Expression;
 
 class LocalAccountType extends AbstractType
 {
+    /** @var string[] */
+    protected $emails;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $accounts = new ArrayCollection($em->getRepository(LocalAccount::class)->findAll());
+        $this->emails = $accounts->map(fn (LocalAccount $account) => (string) $account->getEmail())->toArray();
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('givenname')
-            ->add('familyname')
-            ->add('email', EmailType::class)
+            ->add('givenname', TextType::class, ['required' => true])
+            ->add('familyname', TextType::class, ['required' => true])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'constraints' => [
+                    new Expression([
+                        'expression' => 'value not in emails',
+                        'message' => 'Dit e-mailadres hoort al bij een gebruikersaccount',
+                        'values' => ['emails' => $this->emails],
+                    ]),
+                ],
+            ])
         ;
     }
 
