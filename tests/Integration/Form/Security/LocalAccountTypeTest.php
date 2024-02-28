f75ec0d4608f2cbd945fd2ce5fb3e9369e189527
Peter <peter@sabelzabel.nl>
2024-02-28 17:26:28 +0100

    feat: resolve issue #23

--- tests/Integration/Form/Security/LocalAccountTypeTest.php
+++ tests/Integration/Form/Security/LocalAccountTypeTest.php
@@ -4,6 +4,11 @@ namespace Tests\Integration\Form\Security;
 
 use App\Entity\Security\LocalAccount;
 use App\Form\Security\LocalAccountType;
+use App\Tests\Database\Security\LocalAccountFixture;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
 use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Form\FormFactoryInterface;
@@ -16,6 +21,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class LocalAccountTypeTest extends KernelTestCase
 {
+    protected AbstractDatabaseTool $databaseTool;
+    protected EntityManagerInterface $em;
     protected LocalAccountType $localAccountType;
 
     protected function setUp(): void
@@ -23,8 +30,23 @@ class LocalAccountTypeTest extends KernelTestCase
         parent::setUp();
         self::bootKernel();
 
-        /* @todo Correctly instantiate tested object to use it. */
+        // Get all database tables
+        $this->em = self::getContainer()->get(EntityManagerInterface::class);
+        $cmf = $this->em->getMetadataFactory();
+        $classes = $cmf->getAllMetadata();
+
+        // Write all tables to database
+        $schema = new SchemaTool($this->em);
+        $schema->createSchema($classes);
+
+        // Load database tool
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
         $this->localAccountType = new LocalAccountType();
+
+        // Setup empty data
+        $this->databaseTool->loadFixtures([
+            LocalAccountFixture::class,
+        ]);
     }
 
     protected function tearDown(): void
@@ -52,6 +74,35 @@ class LocalAccountTypeTest extends KernelTestCase
         self::assertTrue($form->isSynchronized());
         self::assertTrue($form->isSubmitted());
         self::assertTrue($form->isValid());
+    }
+
+    public function testUniqueEmailConstraint(): void
+    {
+        $uniqueAccount = new LocalAccount();
+        $duplicateAccount = new LocalAccount();
+
+        $formData = [
+            'givenname' => 'John',
+            'familyname' => 'Doe',
+            'email' => 'john@doe.eye',
+        ];
+
+        /** @var FormFactoryInterface $formfactory */
+        $formfactory = self::getContainer()->get('form.factory');
+        $form = $formfactory->create(LocalAccountType::class, $uniqueAccount, ['csrf_protection' => false]);
+
+        $form->submit($formData);
+        self::assertTrue($form->isSynchronized());
+        self::assertTrue($form->isSubmitted());
+        self::assertTrue($form->isValid());
+        $this->em->persist($uniqueAccount);
+        $this->em->flush();
+
+        $form2 = $formfactory->create(LocalAccountType::class, $duplicateAccount, ['csrf_protection' => false]);
+        $form2->submit($formData);
+        $errors = $form2->getErrors(true);
+        self::assertFalse($form2->isValid());
+        self::assertEquals('This e-mail address is already in use.', $errors->current()->getMessage());
     }
 
     public function testConfigureOptions(): void
