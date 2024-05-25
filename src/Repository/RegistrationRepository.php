60ad1b674dc4abf09b22b60e648d5851cacf5a46
Peter <peter@sabelzabel.nl>
2024-05-25 12:29:46 +0200

    fix: added transferable db column

--- src/Repository/RegistrationRepository.php
+++ src/Repository/RegistrationRepository.php
@@ -51,33 +51,4 @@ class RegistrationRepository extends ServiceEntityRepository
             ->getResult()
         ;
     }
-
-    // /**
-    //  * @return Registration[] Returns an array of Registration objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Registration
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
