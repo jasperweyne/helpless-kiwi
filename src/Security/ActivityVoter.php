da3e1839c7df54ca928231d58eb0a7f4bf6f0506
Peter Sabel (Zabel) <peter@sabelzabel.nl>
2026-09-11 08:35:36 +0200

    chore: added ActivityVoter

---
+++ src/Security/ActivityVoter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Activity\Activity;
+use App\Entity\Security\LocalAccount;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class ActivityVoter extends Voter
+{
+    public const VIEW = 'view_activity';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return self::VIEW === $attribute && $subject instanceof Activity;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        /* @var Activity $activity */
+        $activity = $subject;
+
+        $user = $token->getUser();
+        if ($user instanceof LocalAccount && in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+            return true;
+        }
+
+        return $activity->isVisibleBy($user instanceof LocalAccount ? $user : null);
+    }
+}
